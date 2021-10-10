3df946a7fc9cfe58205df9c691e331e14da2b45d
Trevor Anderson <tbone101@gmail.com>
2021-10-10 09:35:31 -0700

    Made Adjustments

--- StoreProducts/store.py
+++ StoreProducts/store.py
@@ -5,18 +5,30 @@ class Store:
         self.products = []
 
     def add_product(self, new_product):
-        price = input(f"Enter Price for {new_product}: $")
+        price = 3
+        # input(f"Enter Price for {new_product}: $")
         price = float(price)
-        category = input("Enter Category: ")
+        category = "Butcher"
+        # input("Enter Category: ")
         new_product = Products(new_product, price, category)
         self.products.append(new_product)
 
     def sell_product(self, productId):
         for item in self.products:
-            # print(self.products[item].product_id)
             if item.product_id == productId:
                 item.print_info()
                 self.products.remove(item)
+
+        # for item in range(0, len(self.products)):
+        #     print(item)
+        #     print(self.products[item].product_id)
+        #     print(balls)
+        #     if self.products[item].product_id == balls:
+        #         self.products[item].print_info()
+        #         self.products.pop(item)
+        #         return print(self.products)
+        #     else:
+        #         print("Didn't find")
 
     def inflation(self, percent_increase):
         for item in self.products:
